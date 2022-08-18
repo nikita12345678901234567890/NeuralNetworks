a4fbdf23384f359bd8ec6ad00ebb48ec57834719
Nikita Ulianov <person@mectruck.com>
2022-08-17 17:34:35 -0700

    Started game theory

---
+++ GameTheory/GameTheory/MiniMax.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTheory
+{
+    public class MiniMax<T> where T : IGameState<T>
+    {
+        int Minimax(IGameState<T> state, bool isMax)
+        {
+            /*
+            if (state.IsTerminal) return state's value;
+            if (isMax) //if maximizer turn, find greatest value in all the possible turns
+            else //else if minimizer turn, find smallest value in all the possible turns
+            */
+            if (state.IsTerminal)
+            {
+                if (isMax)
+                {
+                    if (state.IsWin) return 1;
+                    if (state.IsTie) return 0;
+                    if (state.IsLoss) return -1;
+                }
+                else
+                {
+                    if (state.IsWin) return -1;
+                    if (state.IsTie) return 0;
+                    if (state.IsLoss) return 1;
+                }
+            }
+            if (isMax)
+            {
+
+            }
+        }
+    }
+
+    public interface IGameState<T> where T : IGameState<T>
+    {
+        int value { get; }
+        bool IsWin { get; }
+        bool IsTie { get; }
+        bool IsLoss { get; }
+        bool IsTerminal { get; }
+        T[] GetChildren();
+    }
+}
