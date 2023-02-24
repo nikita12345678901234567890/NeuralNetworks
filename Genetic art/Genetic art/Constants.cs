44ccdbd801abca5515703aeed1495968c61a8940
Nikita Ulianov <person@mectruck.com>
2023-02-23 17:33:13 -0800

    started

---
+++ Genetic art/Genetic art/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetic_art
+{
+    public static class Constants
+    {
+        public static float mutateColor = 0.5f;//vs mutating points
+    }
+}
---
+++ Genetic art/Genetic art/Triangle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetic_art
+{
+    public class Triangle
+    {
+        Color color;
+        PointF[] points = new PointF[3];
+
+        public Triangle(PointF point0, PointF point1, PointF point2, Color color)
+        {
+            points[0] = point0;
+            points[1] = point1;
+            points[2] = point2;
+
+            this.color = color;
+        }
+
+        public void DrawTriangle(Graphics gfx, float xCoefficent, float yCoefficent)
+        {
+            gfx.FillPolygon(new SolidBrush(color), points);
+        }
+
+        public void Mutate(Random random)
+        {
+            if (random.NextDouble() <= Constants.mutateColor)
+            {
+                switch (random.NextDouble())
+                {
+                    case < 0.25://I love this
+
+                        break;
+
+                    case < 0.5:
+
+                        break;
+
+                    case < 0.75:
+
+                        break;
+
+                    default:
+
+                        break;
+                }
+            }
+            else
+            {
+                
+            }
+        }
+
+        public Triangle Copy()
+        {
+        
+        }
+
+        public static Triangle RandomTriangle(Random random)
+        {
+        
+        }
+    }
+}
